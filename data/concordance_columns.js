d1d65c1ae17eb2ffe64325d32182b0780ded35b6
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2015-08-20 11:56:28 +0200

    Added manually curated data files

---
+++ data/concordance_columns.js
@@ -0,0 +1,61 @@
+var concordanceColumns = [
+  {
+    "column": "Kurzbeschreibung",
+    "sigils": [
+      "headNote"
+    ]
+  }, {
+    "column": "Sigle WA",
+    "sigils": [
+      "idno_wa_faust"
+    ]
+  }, {
+    "column": "Sigle Bohnenkamp",
+    "sigils": [
+      "idno_bohnenkamp"
+    ]
+  }, {
+    "column": "Sigle (andere)",
+    "sigils": [
+      "idno_fischer_lamberg",
+      "idno_landeck"
+    ]
+  }, {
+    "column": "Archiv",
+    "sigils": [
+      "repository",
+    ]
+  }, {
+    "column": "Archivsignatur",
+    "sigils": [
+      "idno_gsa_2",
+      "idno_agad_warszawa",
+      "idno_bb_cologny",
+      "idno_bb_vicenza",
+      "idno_bj_krakow",
+      "idno_bl_london",
+      "idno_bl_oxford",
+      "idno_dla_marbach",
+      "idno_fdh_frankfurt",
+      "idno_mlm_paris",
+      "idno_gm_duesseldorf",
+      "idno_sa_hannover",
+      "idno_sb_berlin",
+      "idno_thlma_weimar",
+      "idno_tml_new_york",
+      "idno_ub_basel",
+      "idno_ub_bonn",
+      "idno_ub_leipzig",
+      "idno_ul_edinburgh",
+      "idno_ul_pennstate",
+      "idno_ul_yale",
+      "idno_veste_coburg"
+    ]
+  }, {
+    "column": "GSA-Signatur (alt)",
+    "sigils": [
+      "idno_gsa_1",
+      "subidno_gsa_1"
+    ]
+  }
+];
---
+++ data/scene_line_mapping.js
@@ -0,0 +1,332 @@
+var sceneLineMapping = [
+  {
+    "rangeStart": 1,
+    "rangeEnd": 32,
+    "title": "Zueignung",
+    "id": "1.1"
+  },
+  {
+    "rangeStart": 33,
+    "rangeEnd": 242,
+    "title": "Vorspiel auf dem Theater",
+    "id": "1.2"
+  },
+  {
+    "rangeStart": 243,
+    "rangeEnd": 353,
+    "title": "Prolog im Himmel",
+    "id": "1.3"
+  },
+  {
+    "rangeStart": 354,
+    "rangeEnd": 807,
+    "title": "Nacht",
+    "id": "1.4"
+  },
+  {
+    "rangeStart": 808,
+    "rangeEnd": 1177,
+    "title": "Vor dem Tor",
+    "id": "1.5"
+  },
+  {
+    "rangeStart": 1178,
+    "rangeEnd": 1529,
+    "title": "Studienzimmer I",
+    "id": "1.6"
+  },
+  {
+    "rangeStart": 1530,
+    "rangeEnd": 2072,
+    "title": "Studienzimmer II",
+    "id": "1.7"
+  },
+  {
+    "rangeStart": 2073,
+    "rangeEnd": 2336,
+    "title": "Auerbachs Keller in Leipzig",
+    "id": "1.8"
+  },
+  {
+    "rangeStart": 2337,
+    "rangeEnd": 2604,
+    "title": "Hexenküche",
+    "id": "1.9"
+  },
+  {
+    "rangeStart": 2605,
+    "rangeEnd": 2677,
+    "title": "Straße. Faust Margarete",
+    "id": "1.10"
+  },
+  {
+    "rangeStart": 2678,
+    "rangeEnd": 2804,
+    "title": "Abend. Ein kleines reinliches Zimmer",
+    "id": "1.11"
+  },
+  {
+    "rangeStart": 2805,
+    "rangeEnd": 2864,
+    "title": "Spaziergang",
+    "id": "1.12"
+  },
+  {
+    "rangeStart": 2865,
+    "rangeEnd": 3024,
+    "title": "Der Nachbarin Haus",
+    "id": "1.13"
+  },
+  {
+    "rangeStart": 3025,
+    "rangeEnd": 3072,
+    "title": "Straße. Faust Mephistopheles",
+    "id": "1.14"
+  },
+  {
+    "rangeStart": 3073,
+    "rangeEnd": 3204,
+    "title": "Garten",
+    "id": "1.15"
+  },
+  {
+    "rangeStart": 3205,
+    "rangeEnd": 3216,
+    "title": "Ein Gartenhäuschen",
+    "id": "1.16"
+  },
+  {
+    "rangeStart": 3217,
+    "rangeEnd": 3373,
+    "title": "Wald und Höhle",
+    "id": "1.17"
+  },
+  {
+    "rangeStart": 3374,
+    "rangeEnd": 3413,
+    "title": "Gretchens Stube",
+    "id": "1.18"
+  },
+  {
+    "rangeStart": 3414,
+    "rangeEnd": 3543,
+    "title": "Marthens Garten",
+    "id": "1.19"
+  },
+  {
+    "rangeStart": 3544,
+    "rangeEnd": 3586,
+    "title": "Am Brunnen",
+    "id": "1.20"
+  },
+  {
+    "rangeStart": 3587,
+    "rangeEnd": 3619,
+    "title": "Zwinger",
+    "id": "1.21"
+  },
+  {
+    "rangeStart": 3620,
+    "rangeEnd": 3775,
+    "title": "Nacht. Straße vor Gretchens Türe",
+    "id": "1.22"
+  },
+  {
+    "rangeStart": 3776,
+    "rangeEnd": 3834,
+    "title": "Dom",
+    "id": "1.23"
+  },
+  {
+    "rangeStart": 3835,
+    "rangeEnd": 4222,
+    "title": "Walpurgisnacht",
+    "id": "1.24"
+  },
+  {
+    "rangeStart": 4223,
+    "rangeEnd": 4398,
+    "title": "Walpurgisnachtstraum",
+    "id": "1.25"
+  },
+  {
+    "rangeStart": 4389,
+    "rangeEnd": 4399,
+    "title": "Trüber Tag. Feld",
+    "id": "1.26"
+  },
+  {
+    "rangeStart": 4399,
+    "rangeEnd": 4404,
+    "title": "Nacht, offen Feld",
+    "id": "1.27"
+  },
+  {
+    "rangeStart": 4405,
+    "rangeEnd": 4612,
+    "title": "Kerker",
+    "id": "1.28"
+  },
+  {
+    "rangeStart": 4613,
+    "rangeEnd": 4727,
+    "title": "Anmutige Gegend",
+    "id": "2.1"
+  },
+  {
+    "rangeStart": 4728,
+    "rangeEnd": 5064,
+    "title": "Saal des Thrones",
+    "id": "2.2"
+  },
+  {
+    "rangeStart": 5065,
+    "rangeEnd": 5986,
+    "title": "Weitläufiger Saal mit Nebengemächern",
+    "id": "2.3"
+  },
+  {
+    "rangeStart": 5987,
+    "rangeEnd": 6172,
+    "title": "Lustgarten",
+    "id": "2.4"
+  },
+  {
+    "rangeStart": 6173,
+    "rangeEnd": 6306,
+    "title": "Finstere Galerie",
+    "id": "2.5"
+  },
+  {
+    "rangeStart": 6307,
+    "rangeEnd": 6376,
+    "title": "Hell erleuchtete Säle",
+    "id": "2.6"
+  },
+  {
+    "rangeStart": 6377,
+    "rangeEnd": 6565,
+    "title": "Rittersaal",
+    "id": "2.7"
+  },
+  {
+    "rangeStart": 6566,
+    "rangeEnd": 6818,
+    "title": "Hochgewölbtes, enges gotisches Zimmer",
+    "id": "2.8"
+  },
+  {
+    "rangeStart": 6819,
+    "rangeEnd": 7004,
+    "title": "Laboratorium",
+    "id": "2.9"
+  },
+  {
+    "rangeStart": 7005,
+    "rangeEnd": 7248,
+    "title": "Pharsalische Felder",
+    "id": "2.10"
+  },
+  {
+    "rangeStart": 7249,
+    "rangeEnd": 7494,
+    "title": "Peneios",
+    "id": "2.11"
+  },
+  {
+    "rangeStart": 7495,
+    "rangeEnd": 8033,
+    "title": "Am oberen Peneios",
+    "id": "2.12"
+  },
+  {
+    "rangeStart": 8034,
+    "rangeEnd": 8274,
+    "title": "Felsbuchten des ägäischen Meers",
+    "id": "2.13"
+  },
+  {
+    "rangeStart": 8275,
+    "rangeEnd": 8487,
+    "title": "Telchinen von Rhodos",
+    "id": "2.14"
+  },
+  {
+    "rangeStart": 8488,
+    "rangeEnd": 9126,
+    "title": "Vor dem Palaste des Menelas zu Sparta",
+    "id": "2.15"
+  },
+  {
+    "rangeStart": 9127,
+    "rangeEnd": 9573,
+    "title": "Innerer Burghof",
+    "id": "2.16"
+  },
+  {
+    "rangeStart": 9574,
+    "rangeEnd": 10038,
+    "title": "[Arkadien]",
+    "id": "2.17"
+  },
+  {
+    "rangeStart": 10039,
+    "rangeEnd": 10344,
+    "title": "Hochgebirg",
+    "id": "2.18"
+  },
+  {
+    "rangeStart": 10345,
+    "rangeEnd": 10782,
+    "title": "Auf dem Vorgebirg",
+    "id": "2.19"
+  },
+  {
+    "rangeStart": 10783,
+    "rangeEnd": 11042,
+    "title": "Des Gegenkaisers Zelt",
+    "id": "2.20"
+  },
+  {
+    "rangeStart": 11043,
+    "rangeEnd": 11142,
+    "title": "Offene Gegend",
+    "id": "2.21"
+  },
+  {
+    "rangeStart": 11143,
+    "rangeEnd": 11287,
+    "title": "Palast",
+    "id": "2.22"
+  },
+  {
+    "rangeStart": 11288,
+    "rangeEnd": 11383,
+    "title": "Tiefe Nacht",
+    "id": "2.23"
+  },
+  {
+    "rangeStart": 11384,
+    "rangeEnd": 11510,
+    "title": "Mitternacht",
+    "id": "2.24"
+  },
+  {
+    "rangeStart": 11511,
+    "rangeEnd": 11603,
+    "title": "Großer Vorhof des Palasts",
+    "id": "2.25"
+  },
+  {
+    "rangeStart": 11604,
+    "rangeEnd": 11843,
+    "title": "Grablegung",
+    "id": "2.26"
+  },
+  {
+    "rangeStart": 11844,
+    "rangeEnd": 12111,
+    "title": "Bergschluchten",
+    "id": "2.27"
+  }
+]
