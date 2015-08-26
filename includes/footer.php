6a17ba93681c9f5083fd45bc58ec8ff30478abea
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-08-26 17:16:02 +0200

    using pure-grid within footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,16 @@
       </div>
     </main>
     <footer>
-      <div id='footer-content' class='footer-content'>
+      <div class="pure-g-r">
+        <div class="pure-u-1-2 pure-fade-50">
           <b>Digitale Faust-Edition</b>
-      </div>
-      <div id="footer-navigation" class="footer-navigation">
-        <a href="help.php">Hilfe</a>
-        <a href="contact.php">Kontakt</a>
-        <a href="imprint.php">Impressum</a>
-        <a href="project.php">Projekt</a>
+        </div>
+        <div class="pure-u-1-2 pure-right pure-fade-50">
+          <a href="help.php">Hilfe</a>
+          <a href="contact.php">Kontakt</a>
+          <a href="imprint.php">Impressum</a>
+          <a href="project.php">Projekt</a>
+        </div>
       </div>
     </footer>
   </body>
