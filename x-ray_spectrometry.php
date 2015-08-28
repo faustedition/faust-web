ecba5bafedf5c4b1193b774a2757ef70f58efc5f
gerritbruening <bruening@faustedition.de>
2015-08-28 15:39:58 +0200

    add file x-ray_spectrometry.php

---
+++ x-ray_spectrometry.php
@@ -0,0 +1,33 @@
+<?php include "includes/header.php"; ?>
+<section class="center pure-g-r">
+    <article class="pure-u-1">
+    <h2>Untersuchung und Datierung von Tinten durch Röntgenfluoreszenzanalyse</h2>
+                <p>Im Januar 2013 ist in Kooperation mit der Bundesanstalt für Materialforschung und -prüfung (BAM) an ausgewählten Handschriften im Goethe- und Schiller-Archiv die mobile Röntgenfluoreszenzanalyse (RFA) zum Einsatz gekommen. Auf der Grundlage der Zusammensetzung der verwendeten Eisengallustinten sollten entstehungsgeschichtliche Erkenntnisse über spezielle Teile des handschriftlichen Materials gewonnen werden. Wie im Fall der ähnlich angelegten Untersuchungen im Rahmen der Marburger Ausgabe der Werke Georg Büchners in den Jahren 2003 bis 2005 (vgl. faust://bibliography/dedner_et_al2005), wurde sowohl eine Datierung von Handschriften insgesamt als auch eine besser fundierte Unterscheidung von Bearbeitungsschichten innerhalb von Handschriften angestrebt, an denen Goethe längere Zeit oder in großen Abständen gearbeitet hat.</p>
+                <p>Das Verfahren beruht auf dem atomaren Prozess der Röntgenfluoreszenz. Zunächst wird ein Atom durch Röntgenstrahlung in einen energetisch höheren Zustand versetzt (Anregung), aus dem das Atom sehr schnell wieder in den energetisch niedrigeren Grundzustand zurückkehrt. Dabei sendet es Röntgenstrahlung aus, deren Energie auf das angeregte Element schließen lässt. Aufgrund der detektierten Energieverteilung der Röntgenfluoreszenzstrahlung können die vorhandenen Elemente identifiziert und auch ihr Mengenverhältnis bestimmt werden (für weitere Angaben zur RFA, zum eingesetzten Gerät sowie zu weiterführender Literatur vgl. faust://bibliography/bruening_et_al2013). Eisengallustinte, wie sie auch Goethe und seine Schreiber verwendeten, wird generell auf die metallischen Bestandteile des zur Herstellung verwendeten Eisenvitriols hin untersucht. Die Differenzierung der Tinten basiert auf der Konzentration der metallischen Nebenbestandteile in Relation zum Hauptbestandteil Eisen. Als am aussagekräftigsten erwiesen sich im Fall der für die RFA ausgewählten Handschriften die relativen Konzentrationen von Kupfer und Kalium.</p>
+                <p><img src="tintenanalyse/media/image1.jpeg" /></p>
+                <p>Abb. 1: Messung mittels mobiler Röntgenfluoreszenzanalyse. GSA 25/W 1567 (WA: III H<sup>1</sup>), Bl. 4r mit über dem Objekt ruhendem Messkopf.</p>
+                <p>An folgenden Handschriften wurden durch Oliver Hahn (Berlin / Hamburg) und Georg Dietz (Dresden) Messungen angestellt:</p>
+                <ul>
+                    <li><p>[[GSA 25/W 1493]]</p></li>
+                    <li><p>[[GSA 25/W 1567]]</p></li>
+                    <li><p>[[GSA 25/W 1589]]</p></li>
+                    <li><p>[[GSA 25/W 1618]]</p></li>
+                    <li><p>[[GSA 25/W 1698]]</p></li>
+                    <li><p>[[GSA 25/W 1729]]</p></li>
+                    <li><p>[[GSA 25/W 1752]]</p></li>
+                    <li><p>[[GSA 25/W 1753]]</p></li>
+                    <li><p>[[GSA 25/W 1800]]</p></li>
+                    <li><p>GSA 28/1053, Bl. 123</p></li>
+                    <li><p>GSA 29/553a,I</p></li>
+                    <li><p>GSA 68/152, St. 63 und 153</p></li>
+                </ul>
+                <p>Die Ergebnisse liegen in Form eines Diagramms vor (vgl. Abb. 2).</p>
+                <p><img src="tintenanalyse/media/image2.png" /></p>
+                <p>Abb. 2: Relative Konzentrationen, ausgedrückt im sog. Fingerprint-Wert W, von Kupfer (Cu) als Funktion der relativen Konzentration von Kalium (K). Dargestellt sind alle Ergebnisse der Messungen.</p>
+                <p>Einen ersten Einblick in die den Messungen zugrundeliegenden Hypothesen und Fragestellungen geben faust://bibliography/bruening_et_al2013; ebd. ein Beispiel für mögliche Datierungen, die mit den Messergebnissen begründet werden können. Die Auswertung der Messergebnisse dauert zurzeit noch an.</p>
+            
+        
+        
+    </article>
+</section>
+<?php include "includes/footer.php"; ?>
