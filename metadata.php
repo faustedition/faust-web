b3fe0359f27a6a1f18c99fa26889d42114a5a3a9
gerritbruening <bruening@faustedition.de>
2015-08-28 14:36:08 +0200

    added file content

--- metadata.php
+++ metadata.php
@@ -1,1 +1,216 @@
-
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="generator" content=
+  "HTML Tidy for HTML5 for Linux version 5.1.2" />
+  <meta http-equiv="content-type" content=
+  "text/html; charset=utf-8" />
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>A_Metadaten</title>
+  <style type="text/css">
+  /*<![CDATA[*/
+  h3.c2 {text-align: left}
+  h2.c1 {text-align: left}
+  /*]]>*/
+  </style>
+</head>
+<body>
+  <header>
+    <div id="headerContent" class="headerContent">
+      <img id="faustLogo" src="img/faustlogo.svg" />
+      <div class="headerSpacer"></div>
+      <nav>
+        <a href="file:///new/archive/">Archive</a> <a href=
+        "file:///new/genesis/work/">Genesis</a> <a href=
+        "file:///new/text/sample">Text</a> <input autocomplete=
+        "off" id="quick-search" placeholder="Search" type="text" />
+      </nav>
+    </div>
+  </header>
+  <div class="content">
+    <div align="center">
+      <p align="right"><a href="K_Hilfe.html">Hilfe</a> <a href=
+      "K_Kontakt.html">Kontakt</a> <a href=
+      "K_Impressum.html">Impressum</a> <a href=
+      "Startseite.html">Projekt</a><br /></p>
+    </div>
+    <div>
+      <h2 class="c1">Beschreibung der Überlieferungsträger
+      (Metadaten)<br /></h2>
+      <div>
+        <h3 class="c2">1. Handschriftenbeschreibung</h3>
+        <p>Zu jeder einzelnen Handschrift gibt es eine Beschreibung
+        mit Angaben (Metadaten) zu Siglen, Aufbewahrungsort,
+        Überlieferung, Blatt- und Papiereigenschaften sowie
+        Konvolutstruktur, die in der Strukturansicht angezeigt
+        werden. Sie ist unterteilt in einen kürzeren Teil mit
+        grundlegenden Angaben zur gesamten Handschrift und ihrer
+        Zuordnung zum Faust-Text und einen zusätzlich einblendbaren
+        längeren Teil mit ausführlichen Detailinformationen.</p>
+        <p>Zwischen einem negativen Befund und dem Vermeiden einer
+        Angabe wird in der Anzeige nicht unterschieden, aber in den
+        zugrundeliegenden Dateien wird die Differenzierung beachtet
+        („none“ für negativen Befund versus „n.s.“ = „not
+        specified“).</p>
+      </div>
+      <div>
+        <h4>1.1. Grundlegende Angaben</h4>
+        <ul>
+          <li>Handschriftensiglen [todo Verlinkung zur
+          Siglenerläuterung]</li>
+          <li>Allgemeine Kurzbeschreibung der Handschrift mit
+          Werkzuordnung</li>
+          <li>Aufbewahrungsort</li>
+          <li>Archiv-Signatur/en</li>
+          <li>Handschriftentyp
+            <ul>
+              <li>Schema</li>
+              <li>Notiz</li>
+              <li>Konzept</li>
+              <li>Arbeitsmundum</li>
+              <li>Mundum</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>1.2. Detailinformationen</h4>
+        <p>Fakultative Angaben stehen in Klammern.</p>
+        <ul>
+          <li>Dokumenteigenschaften
+            <ul>
+              <li>(Überlieferung/Provenienz)</li>
+              <li>(Historische Aufbewahrungsform) – Zeitgenössische
+              Behälter, z. B. Kassetten oder Kapseln, werden
+              ausführlicher beschrieben, etwa mit Angaben zu
+              Entstehungszeit, Stärke, Material und Form.</li>
+              <li>(Einband)</li>
+              <li>(Vorgefundene Zählungen) – Z.&#160;B. Foliierung
+              oder Paginierung.</li>
+              <li>(Erhaltungszustand der Handschrift)</li>
+            </ul>
+          </li>
+          <li>Blatteigenschaften
+            <ul>
+              <li>Blattmaße – Gemessen werden Breite × Höhe in
+              Millimetern. Bei ungleichmäßig geschnittenen Papieren
+              werden die größten und die kleinsten gemessenenen
+              Zahlen angegeben.</li>
+              <li>Format – Es gibt die Zahlenwerte 2 (für Folio), 4
+              (für Quarto), 8 (für Oktav), 12 (für Duodez) und 16
+              (für Sedez).</li>
+              <li>(Bindematerial) – Z.&#160;B. Faden, Leim,
+              Siegellack.</li>
+              <li>(Stichlöcher) – Die Abstände werden vom oberen
+              Blattrand ausgehend in Millimetern angegeben.</li>
+              <li>(Erhaltungszustand des Blattes)</li>
+              <li>(Rand) – Beschnitten oder unbeschnitten.</li>
+            </ul>
+          </li>
+          <li>Papiereigenschaften
+            <ul>
+              <li>Papiersorte – Konzeptpapier, Schreibpapier,
+              Briefpapier, Velinpapier oder Maschinenpapier.</li>
+              <li>Papierfarbe</li>
+              <li>(Steglinienabstand) – Der Abstand der Steglinien
+              wird in Millimetern gemessen.</li>
+              <li>(Papiermühle)</li>
+              <li>Wasserzeichen – Angaben erfolgen als
+              Listenkürzel.</li>
+              <li>Gegenzeichen – Angaben erfolgen als
+              Listenkürzel.</li>
+            </ul>
+          </li>
+          <li>(Eigenschaften von Anbringungen) – Unter Anbringungen
+          sind an- oder aufgeklebte Zettel zu verstehen.
+            <ul>
+              <li>Maße</li>
+              <li>Art der Anbringung – Ein Zettel kann angeklebt,
+              angesteckt oder losgelöst sein.</li>
+              <li>Papiersorte der Anbringung</li>
+              <li>Papierfarbe der Anbringung</li>
+              <li>(Steglinienabstand der Anbringung)</li>
+              <li>(Papiermühle der Anbringung)</li>
+              <li>Wasserzeichen der Anbringung</li>
+              <li>Gegenzeichen der Anbringung</li>
+            </ul>
+          </li>
+          <li>Drucknachweise und Literaturangaben – Kurzbezeichnung
+          und Seitenangabe, bei mehreren Titeln in chronologischer
+          Folge.
+            <ul>
+              <li>(Handschriftenbeschreibung) – Literaturangaben zu
+              Handschriftenbeschreibungen.</li>
+              <li>(Faksimiledruck) – Drucknachweise von
+              Handschriftenfaksimiles.</li>
+              <li>(Textwiedergabe der Handschrift) – Angabe
+              früherer Veröffentlichungsorte.</li>
+              <li>(Forschungsliteratur) – Nachweis von
+              Publikationen mit entstehungsgeschichtlichen
+              Informationen.</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>1.3. Konvolutdarstellung</h4>
+        <p>Die Konvolutdarstellung ist eine schematisierte
+        Gesamtansicht des Überlieferungsträgers (in der Abbildung
+        rechts). Mit ihrer Hilfe lassen sich Handschriften leicht
+        durchblättern.</p><img alt="" src="img/IH32struktur.PNG"
+        height="304" width="624" />
+        <p>Waagerechte Linien entsprechen Blättern. Die obere Kante
+        der Linie stellt die Vorderseite dar, die untere die
+        Rückseite.</p>
+        <p>Die Seiten werden einheitlich und unabhängig von
+        vorhandenen Zählungen (Foliierung, Paginierung)
+        durchgezählt. Nur die Zählung der Vorderseiten wird
+        angezeigt.</p>
+        <p>Im Beispiel entsprechen die Seiten 1 bis 2 einem freien
+        Einzelblatt, die Seiten 3, 4, 9, 10 und 5 bis 8 jeweils
+        einem Doppelblatt. Das zweite Doppelblatt ist in das erste
+        eingelegt und bildet mit ihm eine Lage.<br /></p>
+        <p>Beim Ansteuern der Konvolutansicht werden links die
+        zugehörigen Faksimiles eingeblendet. Rote Farbe zeigt die
+        letzte aktivierte Seitenansicht an. Aktivierbare
+        Seitenpaare sind grau unterlegt. Die Auswahl einer
+        Faksimileseite ruft die jeweilige Handschriftenansicht
+        auf.</p>
+      </div>
+    </div>
+    <div>
+      <h3 id="Drucke_Meta">2. Metadaten der Drucke</h3>
+      <p>Zu jedem Druck gibt es eine Beschreibung mit Angaben
+      (Metadaten) zur Sigle des Drucks in der maßgeblichen
+      Bibliographie von Waltraud Hagen und weiteren Siglen, Nummer
+      des Drucks bei Hagen, gegebenenfalls zum Status als
+      Korrekturexemplar oder Revisionsbogen. Bei
+      Korrekturexemplaren und Revisionsbögen werden auch
+      Aufbewahrungsort und Archivsignatur angegeben.</p>
+      <p>Es folgt die Titelaufnahme mit Angabe des Formats.</p>
+      <p>Aufbewahrungsort und Bibliothekssignatur der
+      digitalisierten sowie weiterer herangezogener Exemplare
+      werden nachgewiesen.</p>
+      <p>Die Literaturangaben am Schluss umfassen:</p>
+      <ul>
+        <li>beschreibende Nachweise des Drucks in
+        wissenschaftlichen Ausgaben und in der
+        Forschungsliteratur;</li>
+        <li>wissenschaftliche Ausgaben, deren Text auf dem
+        betreffenden Druck beruht;</li>
+        <li>photomechanische Nachdrucke;</li>
+        <li>Angaben und Erörterungen über den stemmatologischen Ort
+        eines Drucks in wissenschaftlichen Ausgaben und in der
+        Forschungsliteratur.</li>
+      </ul><br />
+      <br />
+    </div>
+  </div>
+  <footer>
+    <div id="footeContent" class="footerContent">
+      <strong>Digitale Faust-Edition</strong>
+    </div>
+  </footer>
+</body>
+</html>
