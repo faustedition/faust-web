a45ba757ae5aaca645620580031fa79e4787188e
gerritbruening <bruening@faustedition.de>
2015-09-14 19:16:46 +0200

    Change headline of the page

--- project.php
+++ project.php
@@ -1,7 +1,7 @@
       <?php include "includes/header.php"; ?>
       <section class="center pure-g-r">
         <article class="pure-u-1">
-            <h1>Die Ausgabe</h1>
+            <h1>Das Projekt</h1>
             <p>Die Ausgabe basiert auf dem von der Deutschen Forschungsgemeinschaft geförderten Projekt ‚Historisch-kritische Edition von Goethes Faust‘ (2009–2015), das von Anne Bohnenkamp (Freies Deutsches Hochstift, Frankfurt am Main), Silke Henke (Goethe- und Schiller-Archiv, Weimar) und Fotis Jannidis (Julius-Maximilians-Universität Würzburg) geleitet wurde. An der Edition mitgearbeitet haben Gerrit Brüning, Katrin Henzel, Christoph Leijser, Gregor Middell, Dietmar Pravida, Thorsten Vitt und Moritz Wissenbach.</p>
 
 
