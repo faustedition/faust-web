f442c73b3f175737dade5015ed1eeb22d37cc878
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2015-09-27 17:51:33 +0200

    Script for generating the table of sigils

---
+++ sigil-tools/sigils-table.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+from lxml import etree
+import os
+import re
+import pandas as pd
+import argparse
+import sys
+import logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__ if __name__ != '__main__' else sys.argv[0])
+
+NS={"f": "http://www.faustedition.net/ns", "tei": "http://www.tei-c.org/ns/1.0"}
+
+_number = re.compile(r'\b\d+\b')
+def extract_numbers(items):
+    for string in items:
+        for match in _number.findall(string):
+            yield int(match)
+
+
+def write_sigils_table(options):
+    df = pd.DataFrame()
+    docdir = os.path.join(options.directory, 'document')
+    logger.info('Reading metadata from %s ...', docdir)
+    for path, dirs, files in os.walk(docdir):
+        for file in files:
+            filepath = os.path.join(path, file)
+            rel = os.path.relpath(filepath, start=options.directory)
+            uri = "faust://xml/" + rel
+            logger.debug('Reading metadata %s (URI %s)', filepath, uri)
+            meta = etree.parse(filepath)
+            df.at[uri, "type"] = meta.getroot().tag.split('}')[1]
+
+            textTranscripts = meta.xpath('//f:textTranscript', namespaces=NS)
+            for textTranscript in textTranscripts:
+                transcript = os.path.join(options.directory, textTranscript.base[12:] + textTranscript.attrib["uri"])
+                logger.debug('  - textual transcript %s ...', transcript)
+                text = etree.parse(transcript)
+                lines = list(extract_numbers(text.xpath('//tei:l/@n', namespaces=NS)))
+                df.at[uri, 'minVerse'] = min(lines, default=None)
+                df.at[uri, 'maxVerse'] = max(lines, default=None)
+
+            df.at[uri, options.column_name] = ''
+            for idno in meta.xpath('//f:idno', namespaces=NS):
+                if idno.text != 'none':
+                    df.at[uri, idno.attrib["type"]] = idno.text
+
+    df.index.name = 'URI'
+    logger.debug(df.describe())
+    logger.info('Writing %d records to excel file %s, sheet %s', len(df),
+                options.excel_file, options.excel_sheet)
+    df.to_excel(options.excel_file, sheet_name=options.excel_sheet)
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='Tool to deal with the new sigils')
+    parser.add_argument('-d', '--directory', default='/home/tv/Faust',
+                        help='Source directory for XML files')
+    parser.add_argument('-x', '--excel-file', default='sigils.xlsx',
+                        help='Excel file for sigil maintenance')
+    parser.add_argument('-s', '--excel-sheet', default='Sigils',
+                        help='Name of excel sheet to use')
+    parser.add_argument('-n', '--column-name', default='newSigil',
+                        help='Column label for the new sigil column')
+    parser.add_argument('-w', '--write-sigils', action='store_true',
+                        help='Write mode: Write sigils from table to metadata')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='increase verbosity')
+    parser.add_argument('-q', '--quiet', action='count', default=0,
+                        help='decrease verbosity')
+    return parser
+
+def main():
+    options = get_argparser().parse_args()
+    logger.setLevel(logger.getEffectiveLevel()
+                    - (10*options.verbose)
+                    + (10*options.quiet))
+    logger.debug("Options: %s", options)
+    if options.write_sigils:
+        logger.critical('Metadata writing is not implemented yet')
+    else:
+        write_sigils_table(options)
+
+if __name__ == '__main__':
+    main()
