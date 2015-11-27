f5a82e0da1da35b7d7268335e2292e1924133b88
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-11-27 12:41:58 +0100

    adding tab icon

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,9 @@
     <link rel="stylesheet" href="css/pure-min.css">
     <link rel="stylesheet" href="css/pure-custom.css">
     <link rel="stylesheet" href="css/basic_layout.css">
+
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
   </head>
   <body>
     <header>
