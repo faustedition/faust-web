f9a3ed0e3f403dddfb0604f8f548f1e4e574e059
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-11-27 15:13:50 +0100

    style multispectral_imaging.php
    see #5

--- multispectral_imaging.php
+++ multispectral_imaging.php
@@ -2,6 +2,7 @@
 <section class="center pure-g-r">
     <article class="pure-u-1">
         <h1>Multispektrale Bildverarbeitung</h1>
+        <div class="pure-u-2-3 pure-gap">
         <p>In zahlreichen Handschriften werden Bleistiftspuren von späteren Niederschriften in Tinte
             überlagert. Die Bleistiftspuren sind hier wie auch generell oftmals stark verwischt. In
             Kooperation mit Jost Gippert und Manuel Raaf (Frankfurt am Main) wurde multispektrale
@@ -9,14 +10,6 @@
             Deutschen Hochstifts angewendet. Dies diente dem Ziel, die Bleistift- sowohl gegenüber
             der Tintenschicht als auch gegenüber dem Papieruntergrund deutlicher hervortreten zu
             lassen.</p>
-        <figure class="pure-pull-right">
-            <img src="/img/info/multispectral_imaging-image1a.jpg"/>
-            <figcaption>Abb. 1: FDH 30942,17 (Hugo von Hofmannsthal an Gerty von Hofmannsthal, 15. Oktober 1912)</figcaption>
-        </figure>
-        <figure class="pure-pull-right">
-            <img src="/img/info/multispectral_imaging-image1.jpeg"/>
-            <figcaption>Abb. 2: FDH 30942,17 (vgl. auch Abb. 1). Wellenlänge: 440 nm</figcaption>
-        </figure>
         <p>Dabei kamen zwei Leuchten zum Einsatz, deren Strahlung in ihrer Zusammensetzung in etwa
             dem Tageslicht entspricht. Die Strahlungsleistung betrug je 850-Lumen und lag damit im
             Bereich zweier Glühlampen zwischen 60 und 75 Watt. Aus den vom Objekt reflektierten und
@@ -41,14 +34,6 @@
             Kooperation mit Manuel Raaf am 27. und 28. März 2013 angefertigt). Auch diese Tinten
             reflektieren einen Teil des Lichtspektrums stärker als andere und geben so den Blick auf
             die zugrundeliegende Bleistiftschicht frei (Abb. 2 und 3).</p>
-        <figure>
-            <img src="/img/info/multispectral_imaging-image2.png"/>
-            <figcaption>Abb. 2: GSA 25/W 1638 (WA: 2 III H<sup>58</sup>), Vorderseite</figcaption>
-        </figure>
-        <figure>
-            <img src="/img/info/multispectral_imaging-image3.png"/>
-            <figcaption>Abb. 3: vgl. Abb. 2. Wellenlänge: 1000 nm</figcaption>
-        </figure>
         <p>Durch geeignete Kombination der Bilder aus verschiedenen Spektralbereichen konnten die
             Tintenschichten vollständig unsichtbar gemacht werden. Dabei wurde sowohl ein manuelles
             wie auch ein automatisches Verfahren zur Kombination der Spektralaufnahmen getestet.
@@ -58,20 +43,37 @@
             Helligkeitswerte aller Spektralaufnahmen die abhängige und der Helligkeitswert der
             Bleistiftspur die Zielvariable darstellte. Beide Verfahren lieferten vergleichbare
             Ergebnisse.</p>
-        <figure>
-            <img src="hyperspectral_imaging/media/image4.png"/>
-            <figcaption>Abb. 4: Vgl. Abbildung 2. Manuelles Verfahren</figcaption>
-        </figure>
-        <figure>
-            <img src="hyperspectral_imaging/media/image5.png"/>
-            <figcaption>Abb. 5: vgl. Abbildung 2. Automatisches Verfahren</figcaption>
-        </figure>
         <p>Es gelang jedoch nicht, den Kontrast zwischen stark verwischten Bleistiftspuren und dem
             Papieruntergrund so zu verstärken, dass zuvor unlesbare Bleistiftspuren lesbar gemacht
             worden wären. Gegenüber der digitalen Manipulation vorhandener Abbildungen konnten hier
             keine Fortschritte in Form von möglichen Lesungen erzielt werden.</p>
-
-
+        </div>
+        <div class="pure-u-1-3 pure-gap">
+            <figure>
+                <img src="/img/info/multispectral_imaging-image1a.jpg"/>
+                <figcaption>Abb. 1: FDH 30942,17 (Hugo von Hofmannsthal an Gerty von Hofmannsthal, 15. Oktober 1912)</figcaption>
+            </figure>
+            <figure>
+                <img src="/img/info/multispectral_imaging-image1.jpeg"/>
+                <figcaption>Abb. 2: FDH 30942,17 (vgl. auch Abb. 1). Wellenlänge: 440 nm</figcaption>
+            </figure>
+            <figure>
+                <img src="/img/info/multispectral_imaging-image2.png"/>
+                <figcaption>Abb. 2: GSA 25/W 1638 (WA: 2 III H<sup>58</sup>), Vorderseite</figcaption>
+            </figure>
+            <figure>
+                <img src="/img/info/multispectral_imaging-image3.png"/>
+                <figcaption>Abb. 3: vgl. Abb. 2. Wellenlänge: 1000 nm</figcaption>
+            </figure>
+            <figure>
+                <img src="hyperspectral_imaging/media/image4.png"/>
+                <figcaption>Abb. 4: Vgl. Abbildung 2. Manuelles Verfahren</figcaption>
+            </figure>
+            <figure>
+                <img src="hyperspectral_imaging/media/image5.png"/>
+                <figcaption>Abb. 5: vgl. Abbildung 2. Automatisches Verfahren</figcaption>
+            </figure>
+        </div>
     </article>
 </section>
 <?php include "includes/footer.php"; ?>
