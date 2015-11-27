3cc82f360b5e4cff34afb4f414daeabe43301a21
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-11-27 16:53:02 +0100

    show help icon and imprint in header
    we have to show imprint in header in accordance to german law. the link in the footer is not shown on future document-, text- or archive-views.

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="css/document-transcript-highlight-hands.css">
     <link rel="stylesheet" href="css/document-transcript-interaction.css">
     <link rel="stylesheet" href="css/webfonts.css">
+    <link rel="stylesheet" href="css/fontawesome-min.css">
     <link rel="stylesheet" href="css/pure-min.css">
     <link rel="stylesheet" href="css/pure-custom.css">
     <link rel="stylesheet" href="css/basic_layout.css">
@@ -40,6 +41,8 @@
           <li><a href="archives.php">Archiv</a></li>
           <li><a href="chessboard_overview.php">Genese</a></li>
           <li><a href="print/text.html">Text</a></li>
+          <li><a href="help.php"><i class="fa fa-question-circle fa-lg"></i></a></li>
+          <li><a href="imprint.php"><small class="pure-fade-50">Impressum</small></a></li>
           <!-- <li><form class="pure-form"><input autocomplete="off" id="quick-search" placeholder="Suche" type="text"/></form></li> -->
           </ul>
         </nav>
