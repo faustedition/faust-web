62baf213d895525834a043d9200824d7bb04ee3a
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-12-10 16:03:27 +0100

    remove text and adjust graphic
    remove seperation line, adjust positions to align in the center, adjust font size

--- chessboard.php
+++ chessboard.php
@@ -1,24 +1,22 @@
       <?php include "includes/header.php"; ?>
-      <div class="main-content-container">
-        <script>
-          window.addEventListener("DOMContentLoaded", Faust.tooltip.addToTooltipElements);
-        </script>
-        <div id="main-content" class="main-content">
-          <div style="float: left; padding-right: 2em;">
+      <section class="center pure-g-r">
+        
+        <div class="pure-u-1-8"></div>
+        
+        <article class="pure-u-3-4 pure-center">
+            <h1>Genese</h1>
+
+            <h2 class="pure-u-1-2">Faust I</h2>
+            <h2 class="pure-u-1-2">Faust II</h2>
             <?php include "img/chessboard_overview.svg";?>
-          </div>
-          <div align="center" style="padding: 2em 0em">
-            <strong>
-              Werkgenese retrospektiv<br>
-            </strong>
-          </div>
-            <p style="padding-top: 30px;">Die Grafik stellt die Entstehung von Goethes ‚Faust‘ vom abgeschlossenen Werk ausgehend dar.</p>
+        
+        </article>
+        
+        <div class="pure-u-1-8"></div>
 
-            <p>In der linken Hälfte der Übersicht sind all jene Werkteile versammelt, die im Jahr 1808 unter dem Titel ‚Faust. Eine Tragödie‘ erschienen sind. In der rechten Hälfte findet sich eine Übersicht zum ‚Faust&nbsp;II‘. In beiden Hälften entsprechen die angetragenen Jahreszahlen entstehungsgeschichtlich relevanten, an Arbeitsphasen orientierten Daten.</p>
+      </section>
 
-            <p>Die verschiedenen Einfärbungen der Felder zeigen den Ausarbeitungsgrad an. Weiß bedeutet, dass der betroffene Textabschnitt zu dieser Zeit nicht vorliegt. Grau indiziert Unabgeschlossenheit, Schwarz Fertigstellung.</p>
-
-            <p><i>Die Auswahl einer Spalte in einer der beiden Hälften führt zu einer detaillierteren Darstellung.</i></p>
-        </div>
-      </div>
+      <script type="text/javascript">
+        window.addEventListener("DOMContentLoaded", Faust.tooltip.addToTooltipElements);
+      </script>
       <?php include "includes/footer.php"; ?>
