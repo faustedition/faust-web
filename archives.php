6487320839ef4d50164dbdebd92c1d1160d286e1
Sebastian Brümmer <sebastian@produktivbuero.de>
2015-12-11 10:22:21 +0100

    use correct class

--- archives.php
+++ archives.php
@@ -6,7 +6,7 @@
         <article class="pure-u-3-5 pure-center">
             <h1>Archiv</h1>
 
-            <p class="pure-tile">
+            <p class="pure-tiles">
              <a href="archives_locations.php" title="Aufbewahrungsorte" class="pure-button pure-button-tile">Aufbewahrungs&shy;orte</a>
              <a href="archives_manuscripts.php" title="Handschriftenkonkordanz" class="pure-button pure-button-tile">Hand&shy;schriften</a>
              <a href="archives_print.php" title="Konkordanz der Drucke" class="pure-button pure-button-tile">Drucke</a>
