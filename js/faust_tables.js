6de0419d89bc65093ecac78cb0973e4be2401b1a
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2016-01-18 15:12:15 +0100

    Add proper sigil labels.
    Cf. #42

--- js/faust_tables.js
+++ js/faust_tables.js
@@ -6,6 +6,51 @@
 
 
 var createConcordanceTable = function createConcordanceTable(container, repository) {
+
+  // run sigil-labels-json.xsl on sigil-labels.xml (faust-gen-html) to get this list:
+  var sigilLabels = {
+	"idno_faustedition": "Faustedition",
+	"idno_wa_faust": "WA, Faust",
+	"idno_bohnenkamp": "Bohnenkamp",
+	"idno_fischer_lamberg": "Fischer-Lamberg",
+	"idno_landeck": "Landeck",
+	"idno_fa": "FA",
+	"idno_ma": "MA",
+	"idno_wa_gedichte": "WA, Gedichte",
+	"idno_wa_div": "WA, Divan",
+	"idno_hagen": "Hagen",
+	"idno_wa_I_53": "WA I 53",
+	"idno_hagen_nr": "Hagen-Nr.",
+	"idno_aa_ls_helenaank": "AA Ls, Helena-Ankündigung",
+	"idno_wa_helenaank": "WA, Helena-Ankündigung",
+	"idno_aa_wilhelmmeister": "AA, Wilhelm Meister",
+	"idno_wa_mur": "WA, Maximen und Reflexionen",
+	"idno_gsa_1": "GSA",
+	"idno_gsa_2": "GSA",
+	"idno_fdh_frankfurt": "FRA",
+	"idno_dla_marbach": "MAR",
+	"idno_sb_berlin": "BER",
+	"idno_ub_leipzig": "LEI",
+	"idno_ub_bonn": "BON",
+	"idno_veste_coburg": "COB",
+	"idno_gm_duesseldorf": "DUE",
+	"idno_sa_hannover": "HAN",
+	"idno_thlma_weimar": "WEI",
+	"idno_bb_cologny": "COL",
+	"idno_ub_basel": "BAS",
+	"idno_bj_krakow": "KRA",
+	"idno_agad_warszawa": "WAR",
+	"idno_bb_vicenza": "VIC",
+	"idno_bl_oxford": "OXF",
+	"idno_bl_london": "LON",
+	"idno_ul_edinburgh": "EDI",
+	"idno_ul_yale": "YAL",
+	"idno_tml_new_york": "NY",
+	"idno_ul_pennstate": "PEN"
+};
+
+
+
   // remove test and the texts from the other repositories
   documentMetadata.metadata = documentMetadata.metadata.filter(function(metadata) {
     return metadata.text !== "test.xml" && 
@@ -67,7 +112,9 @@ var createConcordanceTable = function createConcordanceTable(container, reposito
 	      if( ( sigil !== "none" ) && ( sigil !== "n.s." ) ) {
 		// next condition: sigil idno_gsa_1 may only be written, if the attached repository is gsa. otherwise mute output of idno_gsa_1 sigil
 		if( !( (sigil_key === "idno_gsa_1") && !(metadata.sigils["repository"] === "gsa") ) ) {
-		  tableElementColumnData.sigils.push({key: sigil_key, value: sigil});
+		  if (concordanceColumns[i].sigils.length > 1) {
+		    tableElementColumnData.sigils.push({key: sigil_key, value: sigil});
+		  }
 		  tableElementColumnData.text = tableElementColumnData.text + metadata.sigils[concordanceColumns[i].sigils[j]];
 		}
 	      }
@@ -209,29 +256,38 @@ var createConcordanceTable = function createConcordanceTable(container, reposito
 	concordanceColumns.forEach(function(currentColumn, columnIndex) {
 	  tableData = document.createElement("td");
 	  var cellData = concordanceData[documentIndex][columnIndex];
+	  // First column: faustedition sigil, explicit link
 	  if (columnIndex == 0) {
 	    var tableDataLink = document.createElement("a");
 	    tableDataLink.href = documentLink;
 	    tableDataLink.classList.add('pure-nowrap');
 	    tableDataLink.appendChild(document.createTextNode(cellData.text));
 	    tableData.appendChild(tableDataLink);
+	  // repo column: Explicit link to repo page
 	  } else if ('repository' in cellData) {
 	    var repoLink = document.createElement("a");
 	    repoLink.href = 'archive_locations_detail.php?id=' + cellData['repository'];
 	    repoLink.appendChild(document.createTextNode(cellData.text));
 	    tableData.appendChild(repoLink);
+	  // sigils in cells that can come from more than one sigil always get tooltips etc.
 	  } else if (cellData.sigils.length > 0) {
 	    tableData.setAttribute('data-sort', cellData.text);
 	    cellData.sigils.forEach(function(sigilData, sigilIndex, sigils) {
 	      var elem = document.createElement('span');
-	      // elem.classList.add('pure-nowrap');
-	      elem.setAttribute('title', sigilData.key); // FIXME use label
+	      elem.classList.add('sigil');
+	      var tooltip;
+	      if (sigilLabels[sigilData.key]) 
+		tooltip = sigilLabels[sigilData.key];
+	      else
+		tooltip = sigilData.key;
+	      elem.setAttribute('title', tooltip);
 	      elem.textContent = sigilData.value;
 	      tableData.appendChild(elem);
 	      if (sigilIndex + 1 < sigils.length) {
 		tableData.appendChild(document.createTextNode(', '));
 	      }
 	    });
+	  // everything else: Just put the text in
 	  } else {
 	    tableData.appendChild(document.createTextNode(cellData.text));
 	  }
