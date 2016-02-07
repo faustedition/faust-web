d4d28a0c9d0476da43725a5bea8e5d0dad6ee707
gerritbruening <bruening@faustedition.de>
2016-02-07 19:41:51 +0100

    Comment out update section in intro

--- intro.php
+++ intro.php
@@ -181,10 +181,10 @@
         <?php // NOTE: see links in imprint ?>
         <p class="pure-fade-50">[Beispiel(e) für Zitieren aus der Ausgabe: Primärquellen, Texte]</p>
         
-        <h2 id="updates">Updates</h2>
+        <!-- <h2 id="updates">Updates</h2>
         <?php // NOTE: see links in index and imprint ?>
         <p class="pure-fade-50">[Download aller verfügbaren Versionen der Ausgabe (oder Teilen
-            davon)]</p>
+            davon)]</p>-->
         
     </article>
 </section>
