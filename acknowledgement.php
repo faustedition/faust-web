51e313b9618dd8eabe5bb7e1be7362390a1357ba
gerritbruening <bruening@faustedition.de>
2016-02-11 18:55:15 +0100

    Delete acknowledgement

--- acknowledgement.php
+++
@@ -1,9 +0,0 @@
-      <?php include "includes/header.php"; ?>
-      <section class="center pure-g-r">
-
-          <article class="pure-u-1">
-          
-          </article>
-
-      </section>
-      <?php include "includes/footer.php"; ?>
