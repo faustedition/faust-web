45c1744822e1c06c7bedcdaa1922749cefbccd7e
gerritbruening <bruening@faustedition.de>
2016-02-11 11:31:56 +0100

    Rework beta-release

--- beta-release.php
+++ beta-release.php
@@ -1,52 +1,54 @@
 <?php include "includes/header.php"; ?>
 <section class="center pure-g-r">
     <article class="pure-u-1">
-        <h1>Die Beta-Version</h1>
+        <h1>Die Beta-Version (Release am 15.2.2016)</h1>
         <p>Die im Februar 2016 veröffentlichte Version der Ausgabe ist noch nicht endgültig, sondern
-            eine Beta-Version. Mit den drei Säulen „Archiv“, „Genese“ und „Text“ sind in der
-            Beta-Version alle grundlegenden Funktionalitäten realisiert.</p>
+            eine Beta-Version. Mit den drei Säulen „Archiv“, „Genese“ und „Text“ sind hier jedoch 
+            bereits alle grundlegenden Funktionalitäten realisiert.</p>
         <p>Die Erscheinung der Version 1.0 ist für das Frühjahr 2017 vorgesehen. Sie soll einige
             Leistungen und Bestandteile enthalten, die in der Beta-Version noch fehlen:</p>
-        <p>Die Ausgabe stellt das komplette <strong>Archiv</strong> der Faust-Handschriften und der
+        <p>Die Ausgabe stellt bereits in der Beta-Version das komplette <strong>Archiv</strong> der Faust-Handschriften und der
             Drucke zu Lebzeiten als Faksimile und in dokumentarischer wie textueller Umschrift samt
-            Metadaten zur Verfügung. Die Abbildungen der Drucke sind noch nicht verfügbar, ebenso
-            fehlen Mehrfachabbildungen von Handschriftenseiten bei Aufklebungen und ähnlichen
+            Metadaten zur Verfügung. Die Abbildungen der Drucke sind jedoch noch nicht verfügbar, ebenso
+            fehlen noch Mehrfachabbildungen von Handschriftenseiten bei Aufklebungen und ähnlichen
             Fällen. Die dokumentarische Transkription weist an einigen Stellen noch unerwünschte
             Abstände und weitere ästhetische Mängel auf, die mit der Version 1.0 behoben werden
-            sollen. Die Texte und Angaben in den Handschriftenbeschreibungen (Metadaten) und ihre
-            Darstellung sind teils noch nicht ausgereift und werden gerade überarbeitet. In der
-            Version 1.0 sollen die vorliegenden Angaben über Datierungen und die relative
-            Chronologie aufgenommen werden. Auch an der Korrektur der Transkriptionen und den daraus 
-            generierten Textdarstellungen wird zurzeit noch gearbeitet. Etwaige Fehler können jederzeit per E-Mail an Gerrit Brüning (<a
+            sollen. Die Texte und Angaben in den Handschriftenbeschreibungen (Metadaten) 
+            werden noch überarbeitet. In der
+            Version 1.0 sollen ergänzend Angaben über Datierungen und die relative
+            Chronologie aufgenommen werden. Die Darstellung der textuellen Transkriptionen 
+            ist automatisch generiert und wird zurzeit noch überprüft. Etwaige Fehler können per E-Mail an Gerrit Brüning (<a
                 href="mailto:bruening@faustedition.de">bruening@faustedition.de</a>) gemeldet
-            werden. Die Transkriptionen werden mit einer der nächsten Beta-Versionen auch in Form der 
-            zugrundeliegenden TEI-XML-Codierung zum Download zur Verfügung gestellt.</p>
-        <p>Im Bereich <strong> Genese</strong> liefert die Betaversion grafische
+            werden.</p>
+        <p>Im Bereich <strong>Genese</strong> liefert die Beta-Version grafische
             Übersichtsdarstellungen und Detailansichten. Das Balkendiagramm, das die
             Gesamtüberlieferung visualisiert, ist zurzeit, wie die Übersichtstabellen der
             handschriftlichen und gedruckten Überlieferung, nach Siglen sortiert. In der Version 1.0
             soll eine chronologische Sortierung angeboten werden.</p>
-        <p>Über die Möglichkeiten paralleler Ansichten verschiedener Fassungen wird nachgedacht.</p>
-        <p>Der vorläufige <strong>Lesetext</strong> enthält eine integrierte zeilensynoptische
-            Darstellung der übrigen Fassungen und Zugang zu den betreffenden Zeugen. Die Betaversion
+        <p>Die Möglichkeit, verschiedene Fassungen frei nach Benutzerwunsch parallel anzuzeigen, befindet sich in der Erprobung.</p>
+        <p>Als <strong>Lesetext</strong> wird in der Beta-Version  eine getreue, unemendierte Wiedergabe des ‚Faust&nbsp;I‘ nach dem Druck 
+        im achten Band der ersten Cotta’schen Gesamtausgabe (A) sowie des ‚Faust&nbsp;II‘ nach der großen Reinschrift (H) angeboten. 
+        Ein konstituierter Lesetext soll in der Version 1.0 folgen. In den Lesetext ist eine zeilensynoptische
+            Darstellung der übrigen Fassungen und der Zugang zu den betreffenden Zeugen integriert. Die Beta-Version
             beschränkt sich hier auf die Darstellung der jeweiligen Endstufen der entsprechenden
             Handschriften. Die Integration der Binnenvarianz, wie sie bislang schon in den
-            Einzelansichten zu jeder Handschrift dargestellt wird, soll noch erfolgen. Außerdem
+            Einzelansichten zu jeder Handschrift dargestellt wird, soll hier noch erfolgen. Außerdem
             sollen die Fassungen in der zeilensynoptischen Darstellung künftig chronologisch
             sortiert werden.</p>
         <p>Die Funktionalität der Ausgabe soll in der Version 1.0 durch eine spezielle
             <strong>Hilfe</strong>-Funktion noch intuitiver erschlossen werden.</p>
         <p>Die <strong>Suche</strong> soll ebenfalls noch wesentlich verbessert werden.</p>
+        <p>Beginnend mit einer der nächsten Beta-Versionen werden die Transkriptionen 
+         in Form der 
+            zugrundeliegenden TEI-XML-Codierung sowie nach und nach auch alle weiteren Inhalte der Edition zum Download bereitgestellt werden.</p>
         <p>Der <strong>Code</strong> der gesamten Applikation wird auf <a href="https://github.com/faustedition"
             >GitHub</a> verwaltet. Eventuelle Fehler oder Mängel der Web-Funktionalität können
             <a href="https://github.com/faustedition/faust-web/issues">hier</a> gemeldet
             werden.</p>
-        <p>Rosemarie Schillemeit gewährte umfassenden Einblick in die Aufzeichnungen im Nachlass
-            <strong> Jost Schillemeits </strong> und stellte ihre Transkription dieser
-            Aufzeichnungen bereitwillig zur Verfügung. Künftig sollen diese Aufzeichnungen in einem
+        <p>Rosemarie Schillemeit gewährte umfassenden Einblick in die Materialien zur Faust-Genese im Nachlass
+            <strong>Jost Schillemeits</strong> und stellte ihre Transkription dieser
+            Aufzeichnungen bereitwillig zur Verfügung. In der Version 1.0 sollen diese Aufzeichnungen in einem
             besonderen Teil der digitalen Ausgabe verfügbar gemacht werden.</p>
-        <br/>
-        <p>Frankfurt am Main, den 15.2.2016</p>
     </article>
 </section>
 <?php include "includes/footer.php"; ?>
