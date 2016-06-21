641e5eeacb273c9eacbccaa8cfc078cd5678aa90
Sebastian Brümmer <sebastian@produktivbuero.de>
2016-06-21 13:48:00 +0200

    logical naming of variable

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,8 @@
   header('Content-Type: text/html; charset=utf-8');
   
   function inurl($substring) { 
-    $referrer = $_SERVER['REQUEST_URI'];
-    return strpos($referrer, $substring) !== false; 
+    $request = $_SERVER['REQUEST_URI'];
+    return strpos($request, $substring) !== false; 
   }
 
   if (!isset($showFooter)) $showFooter = true;
@@ -37,9 +37,9 @@
     <link rel="stylesheet" href="css/pure-custom.css">
     <link rel="stylesheet" href="css/basic_layout.css">
     <link rel="stylesheet" href="css/slick.css">
-
+    
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
   </head>
   <body>
     <header>
