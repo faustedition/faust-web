eb9b6020f4560699daa96f439d40fa3e6dfbb0dd
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2016-06-27 19:56:18 +0200

    Static /text index page

--- text.php
+++ text.php
@@ -5,9 +5,9 @@
         
         <article class="pure-u-3-5 pure-center">
             <p>
-             <a href="archive_locations" class="pure-button pure-button-tile">Aufbewahrungs&shy;orte</a>
-             <a href="archive_manuscripts" class="pure-button pure-button-tile">Hand&shy;schriften</a>
-             <a href="archive_prints" class="pure-button pure-button-tile">Drucke</a>
+             <a href="print/faust1" class="pure-button pure-button-tile">Faust I</a>
+             <a href="print/faust2" class="pure-button pure-button-tile">Faust II</a>
+             <a href="paralipomena" class="pure-button pure-button-tile">Paralipomena</a>
             </p>
         
         </article>
@@ -19,5 +19,5 @@
 
       <script type="text/javascript">
         // set breadcrumbs
-        document.getElementById("breadcrumbs").appendChild(Faust.createBreadcrumbs([{caption: "Archiv"}]));
+        document.getElementById("breadcrumbs").appendChild(Faust.createBreadcrumbs([{caption: "Text"}]));
       </script>
