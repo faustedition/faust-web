11627845fc46a7d724572fb3ef24b9ec1d6fc0aa
gerritbruening <bruening@faustedition.de>
2016-09-22 16:39:23 +0200

    Correct info on mss outside the GSA

--- intro.php
+++ intro.php
@@ -47,7 +47,7 @@
             Der Bestand des GSA umfasst 455 archivalische Dokumente<!-- //f:archivalDocument[//f:repository/text()[contains(.,"gsa")]] 22.9.16 --> 
             mit insgesamt 1800 beschriebenen<!-- 1803 //f:page[ancestor::f:archivalDocument[//f:repository/text()[contains(.,"gsa")]] and child::f:metadata/f:docTranscript/@uri[contains(.,".xml")]] 22.9.16 -->
             Seiten zum ‚Faust‘. 89 Handschriften<!-- -1 (Tesths.) +1 (1 H.1) //f:archivalDocument[//f:repository/text()[not(contains(.,"gsa")) and not(contains(.,"location_unknown"))]] --> mit 600<!-- 604 -32 (Tesths.) + 234 (1 H.1) //f:page[ancestor::f:archivalDocument[//f:repository/text()[not(contains(.,"gsa")) and not(contains(.,"location_unknown"))]] and child::f:metadata/f:docTranscript/@uri[contains(.,".xml")]] 22.9.16 --> beschriebenen Seiten befinden sich im
-            Besitz von 24 weiteren Institutionen<!-- //f:repository[text()[not(contains(.,"location_unknown")) and not(contains(.,"privatbesitz"))]] --> in Deutschland, der Schweiz, Großbritannien, Polen,
+            Besitz von 23 weiteren Institutionen<!-- //f:repository[text()[not(contains(.,"gsa")) and not(contains(.,"location_unknown")) and not(contains(.,"privatbesitz"))]] --> in Deutschland, der Schweiz, Großbritannien, Polen,
             Frankreich, Italien und in den Vereinigten Staaten sowie in Privatbesitz.</p>
             <!-- For human-generated statistical information see Bohnenkamp et al. in JbFDH 2011 and https://docs.google.com/spreadsheets/d/1wCYH1zKNK38QAdT2x01S2pDnkKxpSOU-QocRpRQNcxU/edit#gid=0 -->
         <p>Das <a href="archive">digitale Archiv</a> macht die gesamte relevante Überlieferung
