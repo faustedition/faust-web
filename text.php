4c59dc309e7d0f02f46b8a3959acf3e329cb1f31
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2018-10-10 09:49:58 +0200

    Vorschlag für #521

--- text.php
+++ text.php
@@ -4,9 +4,13 @@
         <div class="pure-u-1-5"></div>
         
         <article class="pure-u-3-5 pure-center">
+            <h1 class="centered">Faust.</h1>
+            <h2 class="centered">Eine Tragödie</h2>
             <p>
-             <a href="print/faust" class="pure-button pure-button-tile">Faust</a>
+             <a href="print/faust" class="pure-button pure-button-tile">Konstituierter Text</a>
              <a href="print/app" class="pure-button pure-button-tile">Apparat</a>
+             <a href="intro_text" class="pure-button pure-button-tile">Editorischer Bericht</a>
+                <br/>
              <a href="paralipomena" class="pure-button pure-button-tile">Paralipomena</a>
             </p>
         
